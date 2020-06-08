f1a22e82d68918263bdec03935e332174c72a26d
Josh R <jriver35@uncc.edu>
2020-06-08 17:19:06 -0400

    Added schemas

---
+++ seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO employee(first_name, last_name)
+VALUES ("John", "Doe"), ("Ben", "Turner"), ("Terra", "Young"), ("John", "Mike"), ("Lola", "Reyes");
+
+INSERT INTO role (title, salary)
+VALUES ("Software Developer", 80000), ("Salesperson", 50000), ("Accountant", 85000), ("Lawyer", 175000);
+
+INSERT INTO department (name)
+VALUES ("Engineering"), ("Sales"), ("Finance"), ("Legal");
